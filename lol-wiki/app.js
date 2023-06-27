94889e2d76d21754f962c8652b5b5586aea9f38e
Andresfrla <125943749+Andresfrla@users.noreply.github.com>
2023-06-26 21:23:06 -0600

    champ detail basic

--- lol-wiki/app.js
+++ lol-wiki/app.js
@@ -20,15 +20,18 @@ require("./config")(app);
 
 const capitalize = require("./utils/capitalize");
 const projectName = "lol-wiki";
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.locals.appTitle = `${capitalize(projectName)} created with IronLauncher`;
 
 // 👇 Start handling routes here
 const indexRoutes = require("./routes/index.routes");
-app.use("/", indexRoutes);
-
 const authRoutes = require("./routes/auth.routes");
+const championRoutes = require("./routes/champion.routes");
+
+app.use("/", indexRoutes);
 app.use("/auth", authRoutes);
+app.use("/champ-detail", championRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
